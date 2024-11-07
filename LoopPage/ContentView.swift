bf581b5e02a35b4474795ae7410071deb63666a6
irving.huang <irving.hy.huang@vivotek.com>
2024-11-07 15:06:35 +0800

    Initial Commit

---
+++ LoopPage/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LoopPage
+//
+//  Created by IrvingHuang on 2024/11/7.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LoopPage/LoopPageApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoopPageApp.swift
+//  LoopPage
+//
+//  Created by IrvingHuang on 2024/11/7.
+//
+
+import SwiftUI
+
+@main
+struct LoopPageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
